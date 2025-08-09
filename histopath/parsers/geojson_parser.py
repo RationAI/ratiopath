330e8a1dfb5acee9958050ee2a6180b708c777ae
Adam Kukučka <524905@mail.muni.cz>
2025-08-09 14:02:20 +0000

    fix: geojson parser

--- histopath/parsers/geojson_parser.py
+++ histopath/parsers/geojson_parser.py
@@ -1,20 +1,14 @@
 """GeoJSON format annotation parser."""
 
-import json
 import re
-from typing import Iterable, cast
+from typing import Any, Iterable
 
-from shapely.geometry import Point, Polygon
+import geojson
+import shapely
+from geojson import Feature, GeoJSON
+from geojson.geometry import Geometry
 
 from histopath.parsers.abstract_parser import AbstractParser
-from histopath.parsers.typing.geojson import (
-    GeoJSONFeature,
-    GeoJSONFeatureCollection,
-    GeoJSONGeometry,
-    GeoJSONGeometryCollection,
-    GeoJSONObject,
-    GeoJSONType,
-)
 
 
 class GeoJSONParser(AbstractParser):
@@ -24,7 +18,110 @@ class GeoJSONParser(AbstractParser):
     This parser supports both polygon and point geometries.
     """
 
-    def _get_filtered_geojson_geometries(self, **kwargs) -> Iterable[GeoJSONGeometry]:
+    def _parse_geometry(self, geometry: dict[str, Any]) -> list[Geometry]:
+        """Parse a GeoJSON geometry into a Geometry object.
+
+        Args:
+            geometry: A dictionary representing a GeoJSON geometry.
+
+        Returns:
+            A Geometry object.
+        """
+        if "type" not in geometry:
+            raise ValueError("Geometry must contain 'type' key")
+
+        if "type" == "GeometryCollection":
+            if "geometries" not in geometry:
+                raise ValueError("GeometryCollection must contain 'geometries' key")
+            geometries = []
+            for geom in geometry["geometries"]:
+                geometries.extend(self._parse_geometry(geom))
+            return geometries
+
+        if "coordinates" not in geometry:
+            raise ValueError("Geometry must contain 'coordinates' key")
+
+        match geometry["type"]:
+            case "Point":
+                return [geojson.Point(geometry["coordinates"], validate=True)]
+            case "Polygon":
+                return [geojson.Polygon(geometry["coordinates"], validate=True)]
+            case "MultiPoint":
+                return [geojson.MultiPoint(geometry["coordinates"], validate=True)]
+            case "MultiPolygon":
+                return [geojson.MultiPolygon(geometry["coordinates"], validate=True)]
+            case _:
+                raise ValueError(f"Unsupported geometry type: {geometry['type']}")
+
+    def _parse_feature(self, feature: dict[str, Any]) -> list[Feature]:
+        """Parse a single GeoJSON feature into a Feature object.
+
+        Args:
+            feature: A dictionary representing a GeoJSON feature.
+
+        Returns:
+            A Feature object.
+        """
+        if "geometry" not in feature or "properties" not in feature:
+            raise ValueError("Feature must contain 'geometry' and 'properties' keys")
+        return [
+            Feature(
+                geometry=geometry,
+                properties=feature["properties"],
+            )
+            for geometry in self._parse_geometry(feature["geometry"])
+        ]
+
+    def _parse_feature_collection(
+        self, feature_collection: dict[str, Any]
+    ) -> list[Feature]:
+        """Parse a GeoJSON feature collection into a list of Feature objects.
+
+        Args:
+            feature_collection: A dictionary representing a GeoJSON feature collection.
+
+        Returns:
+            A FeatureCollection object.
+        """
+        if "features" not in feature_collection:
+            raise ValueError("FeatureCollection must contain 'features' key")
+        features = []
+        for feature in feature_collection["features"]:
+            features.extend(self._parse_feature(feature))
+        return features
+
+    def _get_list_of_features(self, data: GeoJSON) -> list[Feature]:
+        """Get list of features from the GeoJSON and flatten all GeometryCollections.
+
+        Args:
+            data: A GeoJSON object.
+
+        Returns:
+            A FeatureCollection object.
+        """
+        match data["type"]:
+            case "FeatureCollection":
+                return self._parse_feature_collection(data)
+            case "Feature":
+                return self._parse_feature(data)
+            case (
+                "Point"
+                | "MultiPoint"
+                | "Polygon"
+                | "MultiPolygon"
+                | "GeometryCollection"
+            ):
+                return [
+                    Feature(
+                        geometry=geometry,
+                        properties={},
+                    )
+                    for geometry in self._parse_geometry(data)
+                ]
+            case _:
+                raise ValueError("Unsupported GeoJSON type")
+
+    def _get_filtered_geojson_geometries(self, **kwargs) -> Iterable[Geometry]:
         """Get geometries that match the provided filters.
 
         Args:
@@ -34,36 +131,9 @@ class GeoJSONParser(AbstractParser):
             GeoJSONGeometry objects that match the filters.
         """
         with open(self.file_path, "r") as f:
-            data = cast(GeoJSONObject, json.load(f))
+            data = GeoJSON(geojson.load(f))
 
-        # Handle different GeoJSON structures
-        features: list[GeoJSONFeature]
-        geojson_type = GeoJSONType(data["type"])
-        match geojson_type:
-            case GeoJSONType.FEATURE_COLLECTION:
-                features = cast(GeoJSONFeatureCollection, data)["features"]
-            case GeoJSONType.FEATURE:
-                features = [cast(GeoJSONFeature, data)]
-            case (
-                GeoJSONType.POLYGON
-                | GeoJSONType.MULTIPOLYGON
-                | GeoJSONType.POINT
-                | GeoJSONType.MULTIPOINT
-            ):
-                features = [
-                    GeoJSONFeature(
-                        type=geojson_type,
-                        geometry=cast(GeoJSONGeometry, data),
-                        properties={},
-                    )
-                ]
-            case GeoJSONType.GEOMETRY_COLLECTION:
-                features = [
-                    GeoJSONFeature(type=geojson_type, geometry=geometry, properties={})
-                    for geometry in cast(GeoJSONGeometryCollection, data)["geometries"]
-                ]
-            case _:
-                raise ValueError("Unsupported GeoJSON type")
+        features = self._get_list_of_features(data)
 
         filters = {
             key: re.compile(value)
@@ -90,10 +160,9 @@ class GeoJSONParser(AbstractParser):
                     break
 
             if valid:
-                geometry = feature["geometry"]
-                yield cast(GeoJSONGeometry, geometry)
+                yield feature["geometry"]  # this is not GeometryCollection!!!
 
-    def get_polygons(self, **kwargs) -> Iterable[Polygon]:
+    def get_polygons(self, **kwargs) -> Iterable[shapely.Polygon]:
         """Parse polygon annotations from GeoJSON file.
 
         Args:
@@ -104,17 +173,17 @@ class GeoJSONParser(AbstractParser):
         """
         for geometry in self._get_filtered_geojson_geometries(**kwargs):
             match geometry["type"]:
-                case GeoJSONType.POLYGON:
-                    yield Polygon(
+                case "Polygon":
+                    yield shapely.Polygon(
                         geometry["coordinates"][0], geometry["coordinates"][1:]
                     )
-                case GeoJSONType.MULTIPOLYGON:
+                case "MultiPolygon":
                     for coords in geometry["coordinates"]:
-                        yield Polygon(coords[0], coords[1:])
+                        yield shapely.Polygon(coords[0], coords[1:])
                 case _:
                     pass
 
-    def get_points(self, **kwargs) -> Iterable[Point]:
+    def get_points(self, **kwargs) -> Iterable[shapely.Point]:
         """Parse point annotations from GeoJSON file.
 
         Args:
@@ -125,10 +194,10 @@ class GeoJSONParser(AbstractParser):
         """
         for geometry in self._get_filtered_geojson_geometries(**kwargs):
             match geometry["type"]:
-                case GeoJSONType.POINT:
-                    yield Point(geometry["coordinates"])
-                case GeoJSONType.MULTIPOINT:
+                case "Point":
+                    yield shapely.Point(geometry["coordinates"])
+                case "MultiPoint":
                     for coords in geometry["coordinates"]:
-                        yield Point(coords)
+                        yield shapely.Point(coords)
                 case _:
                     pass
