1c5118429259476300518120c4d5f8049b36e6ef
Adam Kukučka <524905@mail.muni.cz>
2025-08-16 11:24:41 +0000

    fix: resolve parsers

--- histopath/parsers/__init__.py
+++ histopath/parsers/__init__.py
@@ -1,9 +1,7 @@
-from histopath.parsers.abstract_parser import AbstractParser
 from histopath.parsers.asap_parser import ASAPParser
 from histopath.parsers.geojson_parser import GeoJSONParser
 
 __all__ = [
-    "AbstractParser",
     "ASAPParser",
     "GeoJSONParser",
 ]
