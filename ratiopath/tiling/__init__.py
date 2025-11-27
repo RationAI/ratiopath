f33262f98fcf84a19ca0bf5998df2e344b1a9926
Jakub Pekár <492788@mail.muni.cz>
2025-11-27 13:04:51 +0000

    feat: tiling

--- ratiopath/tiling/__init__.py
+++ ratiopath/tiling/__init__.py
@@ -1,12 +1,11 @@
 from ratiopath.tiling.annotations import tile_annotations
-from ratiopath.tiling.overlays import overlay_roi, tile_overlay, tile_overlay_overlap
+from ratiopath.tiling.overlays import tile_overlay, tile_overlay_overlap
 from ratiopath.tiling.read_slide_tiles import read_slide_tiles
 from ratiopath.tiling.tilers import grid_tiles
 
 
 __all__ = [
     "grid_tiles",
-    "overlay_roi",
     "read_slide_tiles",
     "tile_annotations",
     "tile_overlay",
