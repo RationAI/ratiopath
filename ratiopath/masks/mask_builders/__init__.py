044fa01aaa2e6ec559d5596141c074fe7ed9e12e
Adam Bajger <469113@mail.muni.cz>
2026-01-09 15:29:03 +0100

    feat: try to migrate changes to RatioPath from RationAI masks

---
+++ ratiopath/masks/mask_builders/__init__.py
@@ -0,0 +1,12 @@
+from ratiopath.masks.mask_builders.mask_builder import (
+    AutoScalingAveragingClippingNumpyMemMapMaskBuilder2D,
+    AveragingScalarUniformTiledNumpyMaskBuilder,
+    MaxScalarUniformTiledNumpyMaskBuilder,
+)
+
+
+__all__ = [
+    "AutoScalingAveragingClippingNumpyMemMapMaskBuilder2D",
+    "AveragingScalarUniformTiledNumpyMaskBuilder",
+    "MaxScalarUniformTiledNumpyMaskBuilder",
+]
