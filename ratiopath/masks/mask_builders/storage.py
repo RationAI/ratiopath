35bfcc85932bf56f0cff282cec8b33c33eca5acc
Adam Bajger <469113@mail.muni.cz>
2026-02-04 13:49:16 +0100

    fix: mypy warnings

--- ratiopath/masks/mask_builders/storage.py
+++ ratiopath/masks/mask_builders/storage.py
@@ -58,7 +58,8 @@ class NumpyMemMapMaskBuilderAllocatorMixin(MaskBuilderABC):
             strict=True,
         ):
             try:
-                mmap._mmap.close()  # Close the memmap to release file handles
+                # Close the memmap to release file handles 
+                mmap._mmap.close()  # type: ignore[attr-defined]
                 # del mmap  # Ensure the memmap object is deleted
                 filepath.unlink(missing_ok=True)
             except Exception as e:
