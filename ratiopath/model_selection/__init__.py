2a29a19569b9a197abfea6bfd6d51e31cf69cafe
Adam Kukučka <adam.kukucka4@gmail.com>
2026-03-03 19:07:26 +0100

    feat: splits (#34)
    * feat: add StratifiedGroupShuffleSplit and train_test_split with group support; update dependencies
    
    * fix: ruff
    
    * fix: PR
    
    * fix: typo
    
    * fix: PR
    
    * fix: docs indentation

---
+++ ratiopath/model_selection/__init__.py
@@ -0,0 +1,10 @@
+from ratiopath.model_selection.split import (
+    StratifiedGroupShuffleSplit,
+    train_test_split,
+)
+
+
+__all__ = [
+    "StratifiedGroupShuffleSplit",
+    "train_test_split",
+]
