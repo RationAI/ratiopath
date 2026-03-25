b1b0ef28ffc3b8c16b68cdd6eb52778adaa67e9e
Šimon Bořil <519192@mail.muni.cz>
2026-03-25 13:53:33 +0000

    fix: fix init

--- ratiopath/parsers/__init__.py
+++ ratiopath/parsers/__init__.py
@@ -1,6 +1,7 @@
 from ratiopath.parsers.asap_parser import ASAPParser
 from ratiopath.parsers.darwin7_json_parser import Darwin7JSONParser
 from ratiopath.parsers.geojson_parser import GeoJSONParser
+from ratiopath.parsers.json_multi_class_mask import JSONMultiClassMask
 
 
-__all__ = ["ASAPParser", "Darwin7JSONParser", "GeoJSONParser"]
+__all__ = ["ASAPParser", "Darwin7JSONParser", "GeoJSONParser", "JSONMultiClassMask"]
